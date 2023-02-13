9e287e25422a05c149eef738464868f468404fac
hung <hungnbc2@gmai.com>
2023-02-13 23:09:09 +0700

    Up_date

---
+++ N_Queen.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int X[100];
+int cot[100], d1[100], d2[100];
+int arr[100][100];
+
+void show()
+{
+    memset(arr, 0, sizeof(arr));
+    for (int i = 1; i <= n; i++)
+    {
+        arr[i][X[i]] = 1;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void N_Queen(int i)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        if (cot[j] == 1 && d1[i - j + n] == 1 && d2[i + j - 1] == 1)
+        {
+            X[i] = j;
+            cot[j] = d1[i - j + n] = d2[i + j - 1] = 0;
+            if (i == n)
+            {
+                show();
+            }
+            else
+            {
+                N_Queen(i + 1);
+            }
+            cot[j] = d1[i - j + n] = d2[i + j - 1] = 1;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= 99; i++)
+    {
+        cot[i] = d1[i] = d2[i] = 1;
+    }
+    N_Queen(1);
+    system("pause");
+}
